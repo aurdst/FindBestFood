c11b25b28b4c6459e9fe0f72488f3ba165b0f11c
unknown <aureliendestailleur@outlook.fr>
2025-01-13 11:20:04 +0100

    update project

--- backend/config/db/database.js
+++ backend/config/db/database.js
@@ -1,3 +1,11 @@
 const { Sequelize } = require('sequelize')
+require('dotenv').config()
 
-const sequelize = new Sequelize('')
+const sequelize = new Sequelize(procces.env.DB, process.env.USER_DBPASSWORD_DB, 
+    {
+        host: 'localhost',
+        dialect: 'mysql',
+    }
+ );
+
+ module.exports = sequelize
---
+++ backend/routes/auth.js
@@ -0,0 +1,39 @@
+const express = require('express')
+const router = express.Router()
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const User= require('../models/User')
+
+// Register
+router.post('/signup', async (req, res) => {
+    const { username, email, password } = req.body
+    try {
+        const hashedPassword = await bcrypt.hash(password, 10)
+        const newUser = await User.create({ username, email, password: hashedPassword })
+        res.status(201).json({ message: 'succes' })
+    }
+    catch (err) {
+        console.log(err)
+        res.status(500).json({ err: 'erreur lors du signup' })
+    }
+})
+
+// Login
+router.post('signin', async (req, res) => {
+    const { email, password } = req.body
+    try {
+        const user = await User.findOne({ where: { email }})
+        if (!user) {
+            console.log(error)
+            res.status(404).json({ message: 'Usr not found' })
+        }
+        const isFound = await bcrypt.compare(password, user.password)
+        if (!isFound) {
+            console.log(error)
+            res.status(401).json({ message: 'Pwd wrong' })
+        }
+    } catch ( err ) {
+        console.log(err)
+        res.status(500).json({ message: 'Erreur lors de la connexion'})
+    }
+})
